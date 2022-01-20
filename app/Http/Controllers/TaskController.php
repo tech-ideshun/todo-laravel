50cb2c2476b28c92f2edd58645e15e0e889d868e
tech-ideshun <tech,ideshun.717@gmail.com>
2022-01-20 15:36:28 +0900

    first commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Task;
+
+class TaskController extends Controller
+{
+    /**
+     * コンストラクタ
+     * 
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * タスク一覧
+     * 
+     * @param Request $request
+     * @return Response
+     */
+    public function index(Request $request)
+    {
+        //$tasks = Task::orderBy('created_at', 'asc')->get();
+        $tasks = $request->user()->tasks()->get();
+        // 認証済みのユーザーであり、そのユーザーが作ったタスクを受け取る
+        return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
+    }
+
+    /**
+     * タスク登録
+     * 
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+        ]);
+
+        // //タスク作成
+        // Task::create([
+        //     'user_id' => 0,
+        //     'name' => $request->name
+        // ]);
+        $request->user()->tasks()->create([
+            'name' => $request->name,
+        ]);
+
+        return redirect('/tasks');
+    }
+
+    /**
+     * タスク削除
+     * 
+     * @param Request $request
+     * @param Task $task
+     * @return Response
+     */
+    public function destroy(Request $request, Task $task)
+    {
+        $this->authorize('destroy', $task);
+
+        $task->delete();
+        return redirect('/tasks');
+    }
+}
